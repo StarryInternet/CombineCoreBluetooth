74e97d7e2b06579147eae98e846f165cd5b7a564
Kevin Lundberg <kevin@klundberg.com>
2022-06-04 22:41:01 -0400

    Add support for swift package index auto-docc-generation and add some doc comments too

--- Package.swift
+++ Package.swift
@@ -5,14 +5,21 @@ import PackageDescription
 
 let package = Package(
   name: "CombineCoreBluetooth",
-  platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
   products: [
     .library(
       name: "CombineCoreBluetooth",
       targets: ["CombineCoreBluetooth"]
     ),
   ],
-  dependencies: [],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+  ],
   targets: [
     .target(
       name: "CombineCoreBluetooth",
